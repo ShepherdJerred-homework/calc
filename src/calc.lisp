bbd2e74c0cb4f37fc78f46e49c88af308025c600
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-09-14 12:52:53 -0500

    Use apply instead of cond

--- src/calc.lisp
+++ src/calc.lisp
@@ -1,16 +1,15 @@
 ; Jerred Shepherd
 (defun calc (x)
-  (let ((operator (second x)) (left (first x)) (right (third x)))
-    (let ((l (if (listp left) (calc left) left)) (r (if (listp right) (calc right) right)))
-      (cond 
-        ((eq operator '+) (+ l r))  
-        ((eq operator '-) (- l r))
-        ((eq operator '*) (* l r))
-        ((eq operator '/) (/ l r))
-        )
-      )
-    )
-  )
+  (let ((operator (second x))
+        (left (first x))
+        (right (third x)))
+    (let ((l (if (listp left) 
+               (calc left) 
+               left)) 
+          (r (if (listp right) 
+               (calc right) 
+               right)))
+      (apply operator l r nil))))
 
 ; (calc '(1 + 2))             ; => 3
 ; (calc '(2 * (1 + 2)))       ; => 6
